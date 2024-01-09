f3687a9943ceb3bf5dc9fdc86b95c27c329f7ecc
DePopee <immanuel.solomonj@gmail.com>
2024-01-09 12:49:52 +0100

    feat / testing

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,44 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Month.*;
+import java.util.List;
+
+import static java.time.Month.JANUARY;
+
+@Configuration
+public class StudentConfig {
+
+    private final StudentRepository repository;
+
+    public StudentConfig(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "mariam@gmail.com",
+                    LocalDate.of(2000, JANUARY, 5)
+
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, JANUARY, 5)
+            );
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+        };
+    }
+}
